cd01890f0031422d1a9beb6f1b244f4f2764c7d7
Hai Bui <haibui.cs@gmail.com>
2023-01-09 22:08:29 +0700

    db config

---
+++ backend/src/routes/userRoutes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request, jsonify
+from src.config.firestore.db import todo_ref
+
+userRoutes = Blueprint("userRoutes", __name__)
+
+
+@userRoutes.route("/list", methods=['GET'])
+def read():
+    """
+        read() : Fetches documents from Firestore collection as JSON
+        todo : Return document that matches query ID
+        all_todos : Return all documents
+    """
+    try:
+        # Check if ID was passed to URL query
+        todo_id = request.args.get('id')    
+        if todo_id:
+            todo = todo_ref.document(todo_id).get()
+            return jsonify(todo.to_dict()), 200
+        else:
+            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
+            return jsonify(all_todos), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
