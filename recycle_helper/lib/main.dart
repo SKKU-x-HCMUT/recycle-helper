d80dcb1ea765a0246642a9677069c9506aac307c
gwonhong <gwonhong.dev@gmail.com>
2023-01-17 20:51:17 +0900

    [DONE]login_page

--- recycle_helper/lib/main.dart
+++ recycle_helper/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MaterialApp(
+    home: LoginPage(),
+  ));
+}
+
 class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
@@ -13,34 +21,37 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: const Text('Login'),
       ),
       body: Form(
         key: _formKey,
         child: Column(
           children: <Widget>[
             TextFormField(
+              //email
               validator: (input) {
-                if (input!.isEmpty) {
+                if (input == null || input.isEmpty) {
                   return 'Please enter an email';
                 }
                 return null;
               },
               onSaved: (input) => _email = input,
-              decoration: InputDecoration(labelText: 'Email'),
+              decoration: const InputDecoration(labelText: 'Email'),
             ),
             TextFormField(
+              //password
               validator: (input) {
-                if (input!.isEmpty) {
+                if (input == null || input.isEmpty) {
                   return 'Please enter a password';
                 }
                 return null;
               },
               onSaved: (input) => _password = input,
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: const InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
             ElevatedButton(
+              //submit button
               onPressed: _submit,
               child: const Text('Submit'),
             ),
