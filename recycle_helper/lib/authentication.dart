c8309d455c35b3ffee661c7469d6b5ba9be767aa
gwonhong <gwonhong.dev@gmail.com>
2023-01-19 21:27:43 +0900

    [ADD]Register Page and Separated dart files

---
+++ recycle_helper/lib/authentication.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _email, _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              //email
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an email';
+                  }
+                  return null; //null means valid
+                },
+                onSaved: (value) => _email = value,
+                decoration: const InputDecoration(labelText: 'Email'),
+              ),
+              //password
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _password = value,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              //submit button
+              ElevatedButton(
+                onPressed: _login,
+                child: const Text('Login'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RegisterPage()));
+                },
+                child: const Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      // calling login API goes here
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Logged in account: \"$_email\"")),
+      ); //Show toast when successfully logged in
+    }
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _email, _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              //email
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an email';
+                  }
+                  return null; //null means valid
+                },
+                onSaved: (value) => _email = value,
+                decoration: const InputDecoration(labelText: 'Email'),
+              ),
+              //password
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _password = value,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              //submit button
+              ElevatedButton(
+                onPressed: _register,
+                child: const Text('Register'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _register() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      // calling register API goes here
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Successfully registered: \"$_email\"")),
+      ); //Show toast when successfully logged in
+    }
+  }
+}
--- recycle_helper/lib/main.dart
+++ recycle_helper/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:recycle_helper/authentication.dart';
+import 'package:recycle_helper/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,79 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "Recycle Helper",
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Login')),
-        body: const Padding(
-          padding: EdgeInsets.all(20),
-          child: LoginWidget(),
-        ),
-      ),
+      home: LoginPage(),
     );
-  }
-}
-
-class LoginWidget extends StatefulWidget {
-  const LoginWidget({Key? key}) : super(key: key);
-
-  @override
-  State<LoginWidget> createState() => _LoginWidgetState();
-}
-
-class _LoginWidgetState extends State<LoginWidget> {
-  final _formKey = GlobalKey<FormState>();
-
-  String? _email, _password;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          //email
-          TextFormField(
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter an email';
-              }
-              return null; //null means valid
-            },
-            onSaved: (value) => _email = value,
-            decoration: const InputDecoration(labelText: 'Email'),
-          ),
-          //password
-          TextFormField(
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter a password';
-              }
-              return null;
-            },
-            onSaved: (value) => _password = value,
-            decoration: const InputDecoration(labelText: 'Password'),
-            obscureText: true,
-          ),
-          //submit button
-          ElevatedButton(
-            onPressed: _login,
-            child: const Text('Login'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _login() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-
-      // calling login API goes here
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Logged in account: \"$_email\"")),
-      ); //Show toast when successfully logged in
-    }
   }
 }
