4144e0f88b8125bfa9a8f2954b0c934bdc697059
anhturhyx <anhtunguyenhuynh1910@gmail.com>
2023-01-23 21:16:07 +0700

    machine learning model call

---
+++ backend/src/controllers/mlcallController.py
@@ -0,0 +1,9 @@
+from src import cardboard
+from src.models.mlcall import predict_image_classification_sample
+
+predict_image_classification_sample(
+    project="203585176079",
+    endpoint_id="7897400596175519744",
+    location="us-central1",
+    filename="inputfilename"
+)
