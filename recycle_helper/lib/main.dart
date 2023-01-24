56a10888ba76b7f345d7a194b64c97de227d96b5
gwonhong <gwonhong.dev@gmail.com>
2023-01-24 23:19:50 +0900

    [ADD]Working camera at the main page

--- recycle_helper/lib/main.dart
+++ recycle_helper/lib/main.dart
@@ -16,8 +16,48 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: "Recycle Helper",
       home: //check login state and go to loginpage or mainpage
-          //if(login_state == false)
+          //if(login_state == false) LoginPage()
           MainPage(),
     );
   }
 }
+
+// class PermissionHandlerPage extends StatefulWidget {
+//   const PermissionHandlerPage({super.key});
+
+//   @override
+//   State<PermissionHandlerPage> createState() => _PermissionHandlerPageState();
+// }
+
+// class _PermissionHandlerPageState extends State<PermissionHandlerPage> {
+//   Future<void> _handlePermission() async {
+//     final status = await Permission.camera.request();
+
+//     if (status.isGranted) {
+//       // Permission has been granted.
+//       print('Permission Granted');
+//     } else if (status.isDenied) {
+//       // We didn't ask for permission yet
+//       // or the permission has been denied before but not permanently.
+//       print('Permission denied');
+//     } else if (status.isPermanentlyDenied) {
+//       // Permission has been denied before but not permanently.
+//       print('Permission Permanently Denied');
+//       await openAppSettings();
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Permission Required')),
+//       body: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: ElevatedButton(
+//           onPressed: _requestCameraPermission,
+//           child: const Text('Request Runtime Camera Permission'),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- recycle_helper/lib/main_page.dart
+++ recycle_helper/lib/main_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class MainPage extends StatefulWidget {
@@ -9,31 +13,177 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  Future<void> requestCamera() async {
-    var status = await Permission.camera.request();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Main')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(children: const [
+          CameraHelper(),
+          //buttons to voucher shop, settings, account page
+        ]),
+      ),
+    );
+  }
+}
 
-    if (status.isGranted) {
-      print('Permission Granted');
-    } else if (status.isDenied) {
-      // We didn't ask for permission yet or the permission has been denied before but not permanently.
-      print('Permission denied');
-    } else if (status.isPermanentlyDenied) {
-      print('Permission Permanently Denied');
-      await openAppSettings();
-    }
+class CameraHelper extends StatefulWidget {
+  const CameraHelper({super.key});
+
+  @override
+  State<CameraHelper> createState() => _CameraHelperState();
+}
+
+class _CameraHelperState extends State<CameraHelper> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<PermissionStatus>(
+      future: Permission.camera.request(),
+      builder: (context, snapshot) {
+        Widget child = const Text('Waiting for camera permission...');
+        if (snapshot.hasData) {
+          PermissionStatus? status = snapshot.data;
+          if (status == null) {
+            child = const Text('Error while acquiring camera permission.');
+          } else {
+            if (status.isGranted) {
+              child = FutureBuilder<List<CameraDescription>>(
+                future: availableCameras(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data == null) {
+                      return const Text('No cameras found.');
+                    } else {
+                      return TakePictureScreen(camera: snapshot.data!.first);
+                    }
+                  } else {
+                    return const Text('Searching for available cameras...');
+                  }
+                },
+              );
+            } else if (status.isDenied) {
+              child = const Text('Camera permission denied.');
+            } else if (status.isPermanentlyDenied) {
+              child = const Text(
+                  'Camera permission permanently denied. Please allow Camera permission through Settings.');
+            }
+          }
+        } else if (snapshot.hasError) {
+          child = Text('Error: ${snapshot.error}');
+        } else {
+          //waiting for the result
+          child = const Text('Waiting for user\'s permission...');
+        }
+        return child;
+      },
+    );
+  }
+}
+
+// A screen that allows users to take a picture using a given camera.
+class TakePictureScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const TakePictureScreen({
+    super.key,
+    required this.camera,
+  });
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+      future: _initializeControllerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // If the Future is complete, display the preview.
+          return CameraPreview(_controller);
+        } else {
+          // Otherwise, display a loading indicator.
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
+
+// IconButton(
+//         icon: const Icon(Icons.camera_alt),
+//         // Provide an onPressed callback.
+//         onPressed: () async {
+//           // Take the Picture in a try / catch block. If anything goes wrong,
+//           // catch the error.
+//           try {
+//             // Ensure that the camera is initialized.
+//             await _initializeControllerFuture;
+
+//             // Attempt to take a picture and get the file `image`
+//             // where it was saved.
+//             final image = await _controller.takePicture();
+
+//             if (!mounted) return;
+
+//             // If the picture was taken, display it on a new screen.
+//             await Navigator.of(context).push(
+//               MaterialPageRoute(
+//                 builder: (context) => DisplayPictureScreen(
+//                   // Pass the automatically generated path to
+//                   // the DisplayPictureScreen widget.
+//                   imagePath: image.path,
+//                 ),
+//               ),
+//             );
+//           } catch (e) {
+//             // If an error occurs, log the error to the console.
+//             print(e);
+//           }
+//         },
+//       );
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ElevatedButton(
-          onPressed: requestCamera,
-          child: const Text('Request Runtime Camera Permission'),
-        ),
-      ),
+      appBar: AppBar(title: const Text('Display the Picture')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Image.file(File(imagePath)),
     );
   }
 }
